eb9ced03e59b8c1c21bff46c75a740a30beecf92
zacharybys <zachary.bys@gmail.com>
2018-12-01 02:17:29 -0500

    Finished recorder component

--- client/src/components/Recorder/Recorder.js
+++ client/src/components/Recorder/Recorder.js
@@ -1,38 +1,62 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import { PropTypes } from 'prop-types';
+import { Button, Header } from 'semantic-ui-react';
+import SpeechRecognition from 'react-speech-recognition';
+
+const propTypes = {
+    // Props injected by SpeechRecognition
+    transcript: PropTypes.string,
+    resetTranscript: PropTypes.func,
+    browserSupportsSpeechRecognition: PropTypes.bool
+}
 
 class Recorder extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          record: false
+          record: true
         }
     }
 
     startRecord = () => {
+        this.props.startListening();
         this.setState({record: true});
     }
 
     stopRecord = () => {
+        this.props.abortListening();
         this.setState({record: false});
     }
 
+
     render() {
+        const { transcript, resetTranscript, recognition, browserSupportsSpeechRecognition, finalTranscript } = this.props;
+
+        if (!browserSupportsSpeechRecognition) {
+            return null
+        }
+
+        // dis in ISO-639
+        recognition.lang = 'en-CN';
+
+        if (finalTranscript) {
+            console.log(finalTranscript);
+            resetTranscript();
+        }
+
         return (
             <div>
                 <Button 
                     color={this.state.record ? "red" : "white"} 
-                    circular icon='microphone'
-                    onClick={this.startRecord}
+                    circular icon={this.state.record ? 'microphone slash' : 'microphone'}
+                    onClick={this.state.record ? this.stopRecord : this.startRecord}
                 />
-                <Button 
-                    color={!this.state.record ? "red" : "white"} 
-                    circular icon='stop'
-                    onClick={this.stopRecord}
-                />
+                <Header>{transcript}</Header>
             </div>
         );
     }
 }
 
-export default Recorder;
+Recorder.propTypes = propTypes
+
+export default SpeechRecognition(Recorder);
