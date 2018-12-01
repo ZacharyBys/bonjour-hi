e03873e324a5141a162d5d9dc756d54efd2ae734
zacharybys <zachary.bys@gmail.com>
2018-11-30 23:43:09 -0500

    sorry

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Home from './components/Home/Home';
+import Home from './views/Home/Home';
 
 class App extends Component {
   render() {
