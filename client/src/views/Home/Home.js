a781eecfe36807839e161b36b3389da90775ddb5
Amarisse <amarissemarie23@gmail.com>
2018-12-01 01:02:26 -0500

    added toggle buttons for login

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
-
+import { Button } from 'semantic-ui-react';
+import { Input } from 'semantic-ui-react';
 class App extends Component {
+  state = {
+    isJoin: true
+  }
+
+toggle(state){
+  this.setState({isJoin: state});
+}
+
+rendering(){
+  if (this.state.isJoin){
+    return(
+    <div>
+      <Input placeholder="Name"></Input>
+      <Input placeholder="Room Code"></Input>
+    </div>
+    );
+  }
+  return(
+    <div>
+      <Input placeholder="Name"></Input>
+    </div>);
+}
   render() {
     return (
         <div>
-            <Header as='h1'>Connectabel</Header>
+            <Header as='h1'>Connectabel Home</Header>
+            {this.rendering()}
+            <Button onClick={() => this.toggle(false)}> Create Room </Button>
+            <Button onClick={() => this.toggle(true)}> Join Room </Button> 
         </div>
     );
   }
